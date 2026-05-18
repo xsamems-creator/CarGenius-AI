fecc3b478f0b9329abfc475089ac0fcb49212634
xsamems-creator <xsamems@gmail.com>
2026-05-18 08:41:48 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ChatBox from "./components/ChatBox";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🚗 CarGenius AI</h1>
+
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
